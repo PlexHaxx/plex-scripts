da733f00380189582ba8bad292d27250c9b595da
ttlequals0 <ttlequals0@users.noreply.github.com>
2015-04-13 01:35:46 +0000

    added deluge PID checking

--- tv.sh
+++ tv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LOGTO=/media/35a8/home/londonfire93/private/deluge/Scripts/logs/log.txt
-# OUTPATH=/media/35a8/home/londonfire93/private/deluge/sorted/$(basename "$TVPATH")
 LOCKFILE=/media/35a8/home/londonfire93/private/deluge/lock.tv
+CURRENT_PID=$(cat /media/35a8/home/londonfire93/.config/deluge/deluged.pid|sha1sum|cut -c 1-10)
 
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
     echo "already running"
@@ -14,25 +14,19 @@ trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
 echo $$ > ${LOCKFILE}
 
 # Process Files 
-(cat /media/35a8/home/londonfire93/private/deluge/lists/tv.list; echo) | while IFS=',' read -r TVPATH TVFOLDER OUTPATH
+(cat /media/35a8/home/londonfire93/private/deluge/lists/tv.list; echo) | while IFS=',' read -r TVPATH TVFOLDER OUTPATH CREATION_PID
 do
-echo "$TVPATH $TVFOLDER $OUTPATH"
-
+echo "$TVPATH $TVFOLDER $OUTPATH $CREATION_PID"
+    if [[ "$CREATION_PID === $CURRENT_PID" ]]; then
 	if [[ "$TVPATH/$TVFOLDER/" == *completed* ]]; then
-
 		echo "Processing $(date) $TVPATH/$TVFOLDER" >> ${LOGTO}
-
 		find "$TVPATH/$TVFOLDER" -name "*.rar" -exec 7z x "{}" -y -o"$TVPATH" \;
-
 		mkdir -p  "${OUTPATH}"
- 
 		find "$TVPATH" -type f \( -iname \*.mkv -o -iname \*.mp4 -o -iname \*.avi \) -exec mv "{}" "${OUTPATH}" \;
- 
 		rm -rf "$TVPATH/$TVFOLDER"
- 
 		echo "Done  $(date) $TVPATH/$TVFOLDER" >>  ${LOGTO}
-
 	fi
+    fi	
 done 
 
 # Remove Lockfile
