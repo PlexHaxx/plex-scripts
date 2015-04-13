6fbb8eb1eff3cf743f9a43cefd7ac64e8ee1795a
ttlequals0 <ttlequals0@users.noreply.github.com>
2015-04-13 02:25:33 +0000

    added PID calculation

--- redirector.sh
+++ redirector.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+PID=$(cat /media/35a8/home/londonfire93/.config/deluge/deluged.pid|sha1sum|cut -c 1-10)
 
 if [[ "$3/$2/" == *completed* ]]; then
 OUTPATH=/media/35a8/home/londonfire93/private/deluge/sorted/$(basename "$3")
-echo "$3,$2,$OUTPATH" >> /media/35a8/home/londonfire93/private/deluge/lists/tv.list
+echo "$3,$2,$OUTPATH,$PID" >> /media/35a8/home/londonfire93/private/deluge/lists/tv.list
 
 fi
 
 if [[ "$3/$2/" == *Movies_Temp* ]]; then
 
-echo "$3,$2" >> /media/35a8/home/londonfire93/private/deluge/lists/movie.list
+echo "$3,$2,$PID" >> /media/35a8/home/londonfire93/private/deluge/lists/movie.list
 
 fi
 
 if [[ "$3/$2/" == *3D* ]]; then
 
-echo "$3,$2" >> /media/35a8/home/londonfire93/private/deluge/lists/3d.list
+echo "$3,$2,$PID" >> /media/35a8/home/londonfire93/private/deluge/lists/3d.list
 
 fi
