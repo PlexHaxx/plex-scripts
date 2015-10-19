2ab88531b94ade47f5f5b066c736dba9d8dd6138
ttlequals0 <ttlequals0@users.noreply.github.com>
2015-10-19 23:31:10 +0000

    updated clenup script

--- clean.sh
+++ clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+killall -9 -u londonfire93 deluged deluge-web
+sleep 10
 rm -rf /media/35a8/home/londonfire93/private/deluge/Scripts/logs/*
 rm -rf /media/35a8/home/londonfire93/private/deluge/completed/*
 rm -rf /media/35a8/home/londonfire93/private/deluge/movies_Comp/*
 rm -rf /media/35a8/home/londonfire93/private/deluge/Movies_Temp/*
 rm -rf /media/35a8/home/londonfire93/private/deluge/3D/*
 rm -rf /media/35a8/home/londonfire93/private/deluge/3d_comp/*
+rm -rf /media/35a8/home/londonfire93/private/deluge/lists/*
+deluged
