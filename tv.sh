617512d739948c853c53132d0030a2ee482a96b4
ttlequals0 <ttlequals0@users.noreply.github.com>
2015-11-14 19:57:35 -0600

    aws

--- tv.sh
+++ tv.sh
@@ -14,15 +14,16 @@ trap 'rm -f ${LOCKFILE}; exit' INT TERM EXIT
 echo $$ > ${LOCKFILE}
 
 # Process Files 
-(cat /media/35a8/home/londonfire93/private/deluge/lists/tv.list; echo) | while IFS=',' read -r TVPATH TVFOLDER OUTPATH CREATION_PID
+(cat /media/35a8/home/londonfire93/private/deluge/lists/tv.list; echo) | while IFS=',' read -r TVPATH TVFOLDER OUTPATH CREATION_PID OUTPATH2
 do
-echo "$TVPATH $TVFOLDER $OUTPATH $CREATION_PID"
+echo "$TVPATH $TVFOLDER $OUTPATH $CREATION_PID $OUTPATH2"
     if [[ "$CREATION_PID" == "$CURRENT_PID" ]]; then
 	if [[ "$TVPATH/$TVFOLDER/" == *completed* ]]; then
 		echo "Processing $(date) $TVPATH/$TVFOLDER" >> ${LOGTO}
 		find "$TVPATH/$TVFOLDER" -name "*.rar" -exec 7z x "{}" -y -o"$TVPATH" \;
 		mkdir -p  "${OUTPATH}"
-		find "$TVPATH" -type f \( -iname \*.mkv -o -iname \*.mp4 -o -iname \*.avi \) -exec mv "{}" "${OUTPATH}" \;
+		find "$TVPATH" -type f \( -iname \*.mkv -o -iname \*.mp4 -o -iname \*.avi \) -exec cp "{}" "${OUTPATH}" \;
+		find "$TVPATH" -type f \( -iname \*.mkv -o -iname \*.mp4 -o -iname \*.avi \) -exec cp "{}" "${OUTPATH2}" \;
 		rm -rf "${TVPATH:?}/${TVFOLDER:?}"
 		echo "Done  $(date) $TVPATH/$TVFOLDER" >>  ${LOGTO}
 	fi
